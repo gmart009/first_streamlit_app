6be2fd872530c3d9d14fa5d64bc47dc3f5809fd4
gmart009 <2755601+gmart009@users.noreply.github.com>
2022-09-12 15:46:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,10 @@ def get_fruityvice_data():
     return fruityvice_normalized
   
 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
 
 
 streamlit.title("My parents new healthy diner")
@@ -44,4 +48,11 @@ try:
         streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
+    
+    
+# Add a button to load the fruit
+if streamlit.button("Get Fruit Load List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_list()
+    streamlit.dataframe(my_data_rows)
 
